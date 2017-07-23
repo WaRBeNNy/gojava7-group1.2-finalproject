a6efbc598717e1f908695f128e5af8d6ca714f82
WaRBeNNy@ <guselnikov_s@i.ua>
2017-07-23 20:13:57 +0300

    Some small commit

---
+++ src/mapper/ObjectMapper.java
@@ -0,0 +1,11 @@
+package mapper;
+
+import writer.JsonWriter;
+
+public class ObjectMapper implements JsonMapper {
+
+    @Override
+    public void write(Object obj, JsonWriter writer) {
+	}
+    
+}
