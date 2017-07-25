632b36f9cf90cf0bfa76746c7082a4cca28b10a2
Kutep85 <genik_630@i.ua>
2017-07-25 14:50:26 +0300

    Add files via upload

--- src/mapper/NumberMapper.java
+++ src/mapper/NumberMapper.java
@@ -5,10 +5,6 @@ import writer.JsonWriter;
 public class NumberMapper implements JsonMapper {
     @Override
     public void write(Object obj, JsonWriter writer) {
-        writer.writeString("\"" + obj.getClass().getSimpleName() + "\"");
-        writer.writePropertySeparator();
-        writer.writeString(" \"");
         writer.writeNumber((Number) obj);
-        writer.writeString(" \"");
     }
 }
