b4631769aa2358bfa069097be680b1216694eb5b
Kutep85 <genik_630@i.ua>
2017-07-29 16:27:24 +0300

    Add files via upload

--- src/mapper/ObjectArrayMapper.java
+++ src/mapper/ObjectArrayMapper.java
@@ -1,10 +1,17 @@
 package mapper;
 
+import serializer.JsonSerializer;
 import writer.JsonWriter;
 
 public class ObjectArrayMapper implements JsonMapper {
     @Override
     public void write(Object obj, JsonWriter writer) {
-
+    	writer.writeArrayBegin();
+    	Object[] arrayObject = (Object[]) obj;
+    	for(int i = 0; i < arrayObject.length; i++){
+    		JsonSerializer jsonSer = new JsonSerializer();
+    		jsonSer.serialize(arrayObject[i]);
+    	}
+    	writer.writeArrayEnd();
     }
 }
