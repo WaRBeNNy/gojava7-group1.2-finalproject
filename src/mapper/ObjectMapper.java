dbca9aded83e9be275e366c63f683f3770c444ae
Kutep85 <genik_630@i.ua>
2017-08-01 17:47:39 +0300

    Add files via upload

--- src/mapper/ObjectMapper.java
+++ src/mapper/ObjectMapper.java
@@ -1,31 +1,32 @@
 package mapper;
 
-import java.io.ByteArrayOutputStream;
-import java.io.ObjectOutputStream;
-import java.io.StringWriter;
+import java.io.Writer;
 import java.lang.reflect.Field;
-import java.util.stream.Stream;
+import java.lang.reflect.Modifier;
+
+import com.sun.xml.internal.bind.v2.schemagen.xmlschema.Annotation;
 
 import serializer.JsonSerializer;
 import writer.JsonWriter;
 
 public class ObjectMapper implements JsonMapper {
 
-
-	@Override
+    @Override
     public void write(Object obj, JsonWriter writer) {
-	// TODO Auto-generated method stub
+    	// TODO Auto-generated method stub
 		JsonSerializer jsonSerializer = JsonSerializer.getInstance();
     		writer.writeObjectBegin();
     		Field[] fields = obj.getClass().getFields();
     		Field.setAccessible(fields, true);
 	        for (Field field : fields) {
 	        	try {
-	        		writer.writeString(field.getName());
-	        		writer.writePropertySeparator();
-	        		Object value = field.get(obj);
-	        		jsonSerializer.serialize(value, writer.getWriter());
-	        		writer.writeSeparator();
+	        		if (isSeriazable(field)) {
+		        		writer.writeString(field.getName());
+		        		writer.writePropertySeparator();
+	        			Object value = field.get(obj);
+	        			jsonSerializer.serialize(value, writer.getWriter());
+		           		writer.writeSeparator();
+	        		}
 				} catch (IllegalStateException | IllegalArgumentException | IllegalAccessException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
@@ -33,4 +34,29 @@ public class ObjectMapper implements JsonMapper {
 	        }
 	        writer.writeObjectEnd();
     	}
+    
+    public boolean isSeriazable(Field field) {
+    	if (Modifier.field.getModifiers().isPrivate()) {
+    		if(isAnnotatable(field)){
+    			return true;
+    		}
+    		else return false;
+    	}
+    	if(Modifier.field.getModifiers().isTransient){
+    		if(isAnnotatable(field)){
+    			return true;
+    		}
+    		else return false;
+    	}
+    	return true;
+    }
+    
+    public boolean isAnnotatable(Field field){
+    	Annotation[] annotations = (Annotation[]) field.getAnnotations();
+    	for (Annotation annotation : annotations){
+    		if (annotation.toString().equals("JsonProperty"));
+    			return true;
+    		}
+    		return false;
+    }
 }
